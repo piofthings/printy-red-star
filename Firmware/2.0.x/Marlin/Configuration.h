82fd50deb9b664f06f28dba031750a06983799a9
Sumit Kumar Maitra <sumitkm@gmail.com>
2022-04-02 19:21:51 +0100

    Changes for BLTouch

--- Firmware/2.0.x/Marlin/Configuration.h
+++ Firmware/2.0.x/Marlin/Configuration.h
@@ -107,7 +107,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 2
+#define SERIAL_PORT 2 
 
 /**
  * Serial Port Baud Rate
@@ -1034,10 +1034,10 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-//#define USE_PROBE_FOR_Z_HOMING
+#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
